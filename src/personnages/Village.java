683c2f563d7655ab024027e71e89068f21e1c2ff
JHNMBO <johan.nzengue-mombo@univ-tlse3.fr>
2023-10-03 09:45:23 +0200

    Fin de sceance

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,13 +1,34 @@
 package personnages;
 
 public class Village {
-	private String nom;
 
-	public Village(String nom) {
+	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
+
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.nbVillageoisMaximum = nbVillageoisMaximum;
+		villageois = new Gaulois[nbVillageoisMaximum];
+		
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 
 	public String getNom() {
 		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois]= gaulois;
+		nbVillageois++;
+	}
+	
+	public String trouverHabitant(int numero) {
+		return nom.villageois[numero];
 	}
 }
