ade30b083bc26c71500d29a3897be5352f2ccb6b
JHNMBO <johan.nzengue-mombo@univ-tlse3.fr>
2023-10-06 17:23:07 +0200

    le village gaulois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -23,12 +23,48 @@ public class Village {
 		return nom;
 	}
 	
+	public String getChef() {
+		return chef.getNom();
+	}
+	
 	public void ajouterHabitant(Gaulois gaulois) {
-		villageois[nbVillageois]= gaulois;
+		villageois[nbVillageois] = gaulois;
 		nbVillageois++;
 	}
 	
-	public String trouverHabitant(int numero) {
-		return nom.villageois[numero];
+	public Gaulois trouverHabitant(int numero) {
+		return villageois[numero];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans "+ getNom() + " du chef " + getChef() + " vivent les légendaires gaulois");
+		for(int i=0;i<nbVillageois;i++)
+		{
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		Les villageois ne sont pas initialisés
+		Chef abraracourcix = new Chef("Abraracourcix",6,village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix",8);
+		village.ajouterHabitant(asterix);
+		Gaulois obelix = new Gaulois("Obélix",25);
+		village.ajouterHabitant(obelix);
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//		Asterix est le deuxieme habitant du village
+		village.afficherVillageois();
+		
+		
+		
+		
+				
+		
 	}
 }
